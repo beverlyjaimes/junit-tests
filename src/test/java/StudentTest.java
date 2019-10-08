b53a9f01bc8067ee54ad9150ff63c9f3780fa014
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-10-08 09:58:30 -0500

    part one finished

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -11,12 +11,37 @@ public class StudentTest {
 
     @Test
     public void testCreateStudent() {
-      Student bob = new Student (111L, "bob");
+      Student bob = new Student (1L, "bob");
       Student joe = null;
       assertNull(joe);
       assertNotNull(bob);
   }
 
+  @Test
+    public void testStudentFields(){
+        Student bob = new Student (1L, "bob");
+        assertSame(1L, bob.getId());
+        assertSame("bob", bob.getName());
+        assertSame(0,bob.getGrades().size());
+  }
+
+  @Test
+    public void testAddGrade(){
+        Student bob = new Student(1L, "bob");
+        bob.addGrade(100);
+        assertSame(100, bob.getGrades().get(0));
+        bob.addGrade(80);
+        assertSame(80, bob.getGrades().get(1));
+
+
+  }
+    @Test
+    public void testAverageGrade(){
+        Student fer = new Student(1L, "bob");
+        fer.addGrade(100);
+        fer.addGrade(80);
+        assertEquals(90, fer.getGradeAverage(), 0);
+    }
 
 
 
